2211e8c0e5d9c568498d9122f67f386992924620
Tayte Choudhury <195010562+t8ctamu@users.noreply.github.com>
2026-09-14 21:38:37 -0500

    Exercise current book forms and delete confirmation in system tests

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -6,36 +6,39 @@ class BooksTest < ApplicationSystemTestCase
   end
 
   test "visiting the index" do
-    visit books_url
-    assert_selector "h1", text: "Books"
+    visit root_url
+    assert_selector "h1", text: "Book Collection"
+    assert_link "Add a book"
   end
 
   test "should create book" do
-    visit books_url
-    click_on "New book"
-
-    fill_in "Title", with: @book.title
+    visit root_url
+    click_on "Add a book"
+    fill_in "Title", with: "A new book"
+    fill_in "Author", with: "A new author"
+    fill_in "Price", with: "12.95"
     click_on "Create Book"
-
     assert_text "Book was successfully created"
-    click_on "Back"
+    assert_text "A new author"
+    assert_text "$12.95"
   end
 
-  test "should update Book" do
+  test "should update book" do
     visit book_url(@book)
-    click_on "Edit this book", match: :first
-
-    fill_in "Title", with: @book.title
+    click_on "Update"
+    fill_in "Title", with: "Updated title"
     click_on "Update Book"
-
     assert_text "Book was successfully updated"
-    click_on "Back"
+    assert_text "Updated title"
   end
 
-  test "should destroy Book" do
+  test "should confirm before deleting book" do
     visit book_url(@book)
-    click_on "Destroy this book", match: :first
-
-    assert_text "Book was successfully destroyed"
+    click_on "Delete"
+    assert_text "Are you sure you want to delete"
+    click_on "Delete Book"
+    assert_text "Book was successfully deleted"
+    assert_current_path root_path
+    assert_not Book.exists?(@book.id)
   end
 end
