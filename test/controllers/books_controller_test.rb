09503e92712ce8481f2a18d75fbc8b28ee2f6f7e
Tayte Choudhury <195010562+t8ctamu@users.noreply.github.com>
2026-09-14 21:36:16 -0500

    Check the collection redirect and book details in controller tests

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -2,47 +2,46 @@ require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @book = books(:one)
+    @book = Book.create!(title: "The Hobbit")
   end
 
-  test "should get index" do
-    get books_url
+  test "all pages render with navigation and the current title" do
+    get root_url
     assert_response :success
-  end
-
-  test "should get new" do
-    get new_book_url
-    assert_response :success
-  end
-
-  test "should create book" do
-    assert_difference("Book.count") do
-      post books_url, params: { book: { title: @book.title } }
+    assert_select "a[href=?]", new_book_path
+    [ new_book_path, edit_book_path(@book), book_path(@book), delete_book_path(@book) ].each do |path|
+      get path
+      assert_response :success
+      assert_select "a[href=?]", root_path
     end
-
-    assert_redirected_to book_url(Book.last)
+    get edit_book_path(@book)
+    assert_select "input[name=?][value=?]", "book[title]", @book.title
   end
 
-  test "should show book" do
-    get book_url(@book)
-    assert_response :success
+  test "create update and delete persist and show notices on home" do
+    assert_difference("Book.count", 1) { post books_url, params: { book: { title: "Dune" } } }
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "[role=status]", "Book was successfully created."
+    book = Book.order(:id).last
+    patch book_url(book), params: { book: { title: "Dune Messiah" } }
+    assert_redirected_to root_url
+    assert_equal "Dune Messiah", book.reload.title
+    follow_redirect!
+    assert_select "[role=status]", "Book was successfully updated."
+    assert_no_difference("Book.count") { get delete_book_url(book) }
+    assert_difference("Book.count", -1) { delete book_url(book) }
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "[role=status]", "Book was successfully deleted."
   end
 
-  test "should get edit" do
-    get edit_book_url(@book)
-    assert_response :success
-  end
-
-  test "should update book" do
-    patch book_url(@book), params: { book: { title: @book.title } }
-    assert_redirected_to book_url(@book)
-  end
-
-  test "should destroy book" do
-    assert_difference("Book.count", -1) do
-      delete book_url(@book)
-    end
-
-    assert_redirected_to books_url
+  test "blank title is rejected without losing an existing book" do
+    assert_no_difference("Book.count") { post books_url, params: { book: { title: " " } } }
+    assert_response :unprocessable_entity
+    assert_select "[role=alert]"
+    patch book_url(@book), params: { book: { title: "" } }
+    assert_response :unprocessable_entity
+    assert_equal "The Hobbit", @book.reload.title
   end
 end
